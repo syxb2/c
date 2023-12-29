64084ceeb040d9681b23ed7bb3725b7a86bb967e
syxb2 <578767478@qq.com>
2023-12-29 20:45:53 +0800

    strStr

--- COnline/Practice/leetcode/strStr.c
+++ COnline/Practice/leetcode/strStr.c
@@ -1,10 +1,69 @@
 #include <stdio.h>
 
+/*
 int strStr(char* haystack, char* needle) {
-  ;
+  char *temp = needle;
+  int n = 0;
+  while (*haystack != '\0' && *needle != '\0') {
+    while (*needle != *haystack) {
+      ++haystack;
+      ++n;
+      if (*haystack == '\0' && *needle != '\0') {
+        break;
+      }
+    }
+    int a = 0;
+    while (*needle == *haystack && *haystack != '\0') {
+      ++haystack;
+      ++needle;
+      ++a;
+    }
+    if (*haystack == '\0' && *needle != '\0') {
+      break;
+    }
+    if (*needle == '\0') {
+      return n;
+    }
+    else {
+      needle = temp;
+      n += a;
+    }
+  }
+  return -1;
+}
+*/
+
+int strStr(char* haystack, char* needle) {
+  char *tneedle = needle;
+  char *thaystack = haystack;
+  int n = 0;
+  while (*haystack != '\0') {
+    if (*haystack == *needle) {
+      while (*haystack == *needle && *needle != '\0') {
+        ++haystack;
+        ++needle;
+        if (*haystack == '\0' && *needle != '\0') break;
+      }
+      if (*needle == '\0') {
+        return n;
+      }
+      else {
+        needle = tneedle;
+      }
+    }
+    thaystack += 1;
+    haystack = thaystack;
+    ++n;
+  }
+  return -1;
 }
 
 int main() {
+  char *h = "mississippi";
+  char *n = "issip";
+  // strstr(h, n);
+  printf("%d\n", strStr(h, n));
+  // printf("%c\n", a[1]);
 
   return 0;
 }
