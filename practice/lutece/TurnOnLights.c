a2b6b2904def5b5f6e930d7be6d9014d52e12044
syxb2 <578767478@qq.com>
2024-03-18 16:36:33 +0800

    ADD TurnOnLights

--- practice/lutece/TurnOnLights.c
+++ practice/lutece/TurnOnLights.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main() {
+    int N = 0;
+    scanf("%d", &N);
+    for (int a = 0; a < N; ++a) {
+        int n = 0;
+        int k = 0;
+        scanf("%d %d", &n, &k);
+        for (int i = 1; i <= n; ++i) {
+            int s = 1;
+            for (int j = 2; j <= i && j <= k; ++j) {
+                if (i % j == 0) {
+                    if (s == 0) {
+                        s = 1;
+                    }
+                    else {
+                        s = 0;
+                    }
+                }
+            }
+            if (s == 1) {
+                printf("%d ", i);
+            }
+        }
+        printf("\n");
+    }
 
     return 0;
 }
